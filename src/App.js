0f7e7a5a788027a02fbc5b8aceab1362b4aadc46
asgoh05 <asgoh05@gmail.com>
2022-12-25 00:34:20 +0900

    Add Main Page and Gallery Page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
-import MainPage from "./components/MainPage";
+import MainPage from "./components/MainPage/MainPage";
+import GalleryPage from "./components/GalleryPage/GalleryPage";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <MainPage />
+        <GalleryPage />
       </header>
     </div>
   );
